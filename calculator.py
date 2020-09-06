48a956bbffb8210ac56cbe1f18adda82a60216fb
MC-Dmitrievich <maximiliana2392@gmail.com>
2020-09-06 21:17:29 +0300

    First homework

---
+++ calculator.py
@@ -0,0 +1,26 @@
+a = float(input())
+zn = input()
+b = float(input())
+if zn == '+':
+    print(a + b)
+if zn == '-':
+    print(a - b)
+if zn == '*':
+    print(a * b)
+if zn == '/':
+    if b != 0:
+        print(a / b)
+    else:
+        print('Деление на 0!')
+if zn == 'mod':
+    if b != 0:
+        print(a % b)
+    else:
+        print('Деление на 0!')
+if zn == 'pow':
+    print(a ** b)
+if zn == 'div':
+    if b != 0:
+        print(a // b)
+    else:
+        print('Деление на 0!')
