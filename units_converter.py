c6e03c696ab877e6f98fad3617c8b1ec6e3b7eb7
MC-Dmitrievich <maximiliana2392@gmail.com>
2020-09-22 18:38:19 +0300

    Second_homework_2

--- units_converter.py
+++ units_converter.py
@@ -6,6 +6,6 @@ print('Доступные: lea, mi, yd, ft, in')
 measure_2 = str(input())
 print('Сколько?')
 num_begin = int(input())
-dict_to_measures = {'lea': 190080, 'mi': 63360, 'yd': 36, 'ft': 12, 'in': 1}
+dict_to_measures = {'lea': 190081, 'mi': 63360, 'yd': 36, 'ft': 12, 'in': 1}
 num_end = num_begin * dict_to_measures[measure_1] / dict_to_measures[measure_2]
 print(num_end)
